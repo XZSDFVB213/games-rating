0755febf6c9360cf1db5684bc8db0447a827064a
XZSDFVB213 <121311205+XZSDFVB213@users.noreply.github.com>
2024-05-26 15:43:38 +0300

    Comment to commit

--- appModules/http-utils/index.js
+++
@@ -1,8 +0,0 @@
-const mimeTypes = require('../http-utils/mime-types')
-const staticFile = require('../http-utils/static-file')
-const parseBody = require('../http-utils/parse-body')
-module.exports = {
-  mimeTypes,
-  staticFile,
-  parseBody
-};
